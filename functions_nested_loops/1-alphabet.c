b2a12df3db00e4ca73a4623ddc5818d68937d0f0
Albaryys <a.tily.pro@gmail.com>
2023-02-21 03:26:36 -0800

    2

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+/*
+ * File: 1-alphabet.c
+ * Author: Arnaud Tily
+ */
+
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+ * Return: nothing
+ */
+void print_alphabet(void)
+{
+	char a;
+
+	a = 'a';
+
+	while (a <= 'z')
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar('\n');
+}
