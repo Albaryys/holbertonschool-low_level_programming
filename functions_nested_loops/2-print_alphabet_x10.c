a5c5984cf2529d81a826e432b91be8ebdfe1b3d4
Albaryys <a.tily.pro@gmail.com>
2023-02-21 04:09:07 -0800

    day2week3

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,29 @@
+/*
+ * File: 2-print_alphabet_x10.c
+ * Auth: Arnaud Tily
+ */
+
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
+ */
+void	print_alphabet_x10(void)
+{
+	int	i;
+	char	a;
+
+	a = 'a';
+	i = 0;
+
+	while (i++ <= 9)
+	{
+        	while (a <= 'z')
+        	{
+                	_putchar(a);
+                	a++;
+        	}
+		_putchar('\n');
+		a = 'a';
+	}
+}
