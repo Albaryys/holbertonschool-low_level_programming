1877f99f6cbf1e0f6b7f23cc1187c49c7513c48c
Albaryys <a.tily.pro@gmail.com>
2023-02-21 05:31:13 -0800

    day2week3

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+/*
+ * File: 4-isalpha.c
+ * Auth: Arnaud Tily
+ */
+
+#include "main.h"
+
+/**
+ * _isalpha - Checks if a character is alphabetic.
+ * @c: The character to be checked.
+ *
+ * Return: 1 if character is letter, lowercase or uppercase, 0 otherwise.
+ */
+int	_isalpha(int c)
+{
+	if ((c <= 'Z' && c >= 'A') || (c <= 'z' && c >= 'a'))
+		return (1);
+	else
+		return (0);
+}
