9a09f7a9ed2f9e2d07fb673eabb49b2ad7459a1b
Albaryys <a.tily.pro@gmail.com>
2023-02-21 04:13:34 -0800

    day2wee3

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -18,11 +18,11 @@ void	print_alphabet_x10(void)
 
 	while (i++ <= 9)
 	{
-        	while (a <= 'z')
-        	{
-                	_putchar(a);
-                	a++;
-        	}
+		while (a <= 'z')
+		{
+			_putchar(a);
+			a++;
+		}
 		_putchar('\n');
 		a = 'a';
 	}
