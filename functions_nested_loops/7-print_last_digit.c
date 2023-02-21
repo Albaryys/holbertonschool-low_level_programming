671159414dfd9381c97fd52e8fc8b28ef83d1092
Albaryys <a.tily.pro@gmail.com>
2023-02-21 06:42:16 -0800

    day2week3

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,24 @@
+/*
+ * File: 7-print_last_digit.c
+ * Auth: Arnaud Tily
+ */
+
+#include "main.h"
+
+/**
+ * print_last_digit - Prints the last digit of a number.
+ * @n: The number in question.
+ *
+ * Return: Value of the last digit.
+ */
+int print_last_digit(int n)
+{
+	int l;
+
+	l = n % 10;
+
+	if (l < 0)
+		l *= -1;
+	_putchar(l + '0');
+	return (l);
+}
