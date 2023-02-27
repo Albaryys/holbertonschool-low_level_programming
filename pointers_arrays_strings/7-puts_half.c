f3d25dfe9b6f427341862e733041b45ef9fdaa74
Albaryys <a.tily.pro@gmail.com>
2023-02-27 05:09:06 -0800

    completed

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+/*
+ * File: 7-puts_half.c
+ * Auth: Arnaud Tily
+ */
+
+#include "main.h"
+
+/**
+ * puts_half - Prints half of a string.
+ * @str: The string to be printed.
+ */
+void puts_half(char *str)
+{
+	int	lenght = 0, i;
+
+	while (str[lenght])
+		lenght++;
+
+	for (i = (lenght + 1) / 2; i < lenght; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
