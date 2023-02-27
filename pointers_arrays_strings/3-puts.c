f8ad5f69cec8d499a5f09060878c905ce136e6cc
Albaryys <a.tily.pro@gmail.com>
2023-02-27 01:21:02 -0800

    complete

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+/*
+ * File: 3-puts.c
+ * Auth: Arnaud Tily
+ */
+
+#include "main.h"
+
+/**
+ * _puts - Prints a string to stdout.
+ * @str: The string to be printed.
+ */
+void _puts(char *str)
+{
+	int	i = 0;
+
+	while (str[i] != '\0')
+		_putchar (str[i]);
+		i++;
+	_putchar ('\n');
+}
