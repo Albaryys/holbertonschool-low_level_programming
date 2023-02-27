3767fdfd2a6132563cd96d7e184aae6bb5e208f3
Albaryys <a.tily.pro@gmail.com>
2023-02-27 00:53:43 -0800

    complete

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,24 @@
+/*
+ * File: 2-strlen.c
+ * Auth: Arnaud Tily
+ */
+
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string.
+ * @str: The string to get the length of.
+ *
+ * Return: The length of @str.
+ */
+int _strlen(char *s)
+{
+	int	len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return len;
+}
