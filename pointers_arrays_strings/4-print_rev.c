519890165cbb07e12cd51ad22bacf692427a232c
Albaryys <a.tily.pro@gmail.com>
2023-02-27 01:56:24 -0800

    completed

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+/*
+ * File: 4-print_rev.c
+ * Auth: Arnaud Tily
+ */
+
+#include "main.h"
+
+/**
+ * print_rev - Prints a string in reverse.
+ * @s: The string to be printed.
+ */
+void print_rev(char *s)
+{
+	int len = 0, index = 0;
+
+	while (s[index++])
+		len++;
+
+	for (index = len - 1; index >= 0; index--)
+		_putchar(s[index]);
+
+	_putchar('\n');
+}
